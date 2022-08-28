3fefedbf91c921c2d7675df3e12588f0e87478e8
brseghese <brseghese@gmail.com>
2022-08-28 19:02:21 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O título do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'alterar': {
+      console.log('Chamou alterar', action.payload);
+      return { ...state, title: action.payload };
+    }
+
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { title, body, counter } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'alterar',
+            payload: new Date().toLocaleDateString('pt-BR'),
+          })
+        }
+      >
+        Alterar
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Inverter</button>
+    </div>
   );
 }
 
