2780121ad5eb130fc26c8a01de42d592edb412f9
brseghese <brseghese@gmail.com>
2022-09-01 05:50:14 -0300

    organização com useEffect, useReducer, useContext e useRef

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,32 @@
+import { useContext, useEffect, useRef } from 'react';
+import { loadPosts } from '../../contexts/PostsProvider/actions';
+import { PostsContext } from '../../contexts/PostsProvider/context';
+
+export const Posts = () => {
+  const isMounted = useRef(true);
+  const postsContext = useContext(PostsContext);
+  const { postsState, postsDispatch } = postsContext;
+
+  useEffect(() => {
+    loadPosts(postsDispatch).then((dispatch) => dispatch());
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [postsDispatch]);
+
+  return (
+    <div>
+      <h1>POSTS</h1>
+      {postsState.loading && (
+        <p>
+          <strong>Carregando posts...</strong>
+        </p>
+      )}
+
+      {postsState.posts.map((p) => (
+        <p key={p.id}>{p.title}</p>
+      ))}
+    </div>
+  );
+};
